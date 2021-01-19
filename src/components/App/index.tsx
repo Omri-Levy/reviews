f514b30db0d39216e4bf80a353d69243ef7e81a8
Omri-Levy <61207713+Omri-Levy@users.noreply.github.com>
2021-01-19 03:12:04 +0200

    Initial commit

---
+++ src/components/App/index.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react';
+
+const App: React.FunctionComponent = () => {
+	return (
+		<div className={'App'}>
+			<h1>Hello from App.</h1>
+		</div>
+	);
+};
+
+export default App;
